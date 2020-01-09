8a63cc93fac0e7bbae5b55879456c6d7a59e9620
Eztena <victor_jesus_agullo_apps1ma1819@cev.com>
2020-01-09 13:13:52 +0100

    Iniciado el request con alamofire

--- Bienestapp/LoginController.swift
+++ Bienestapp/LoginController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginController: UIViewController {
 
+    @IBOutlet weak var nameEntry: UITextField!
+    @IBOutlet weak var mailEntry: UITextField!
+    @IBOutlet weak var passEntry: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func senderButton(_ sender: UIButton) {
+        var name = nameEntry.text
+        var email = mailEntry.text
+        var password = passEntry.text
+        request()
+    }
+    
 }
 
---
+++ Bienestapp/MainController.swift
@@ -0,0 +1,9 @@
+//
+//  MainController.swift
+//  Bienestapp
+//
+//  Created by alumnos on 09/01/2020.
+//  Copyright © 2020 Víctor. All rights reserved.
+//
+
+import Foundation
