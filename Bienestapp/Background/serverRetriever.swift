201bf6c312c9de20acc71f604034867e027bc8ac
Víctor <38455769+Victor-agullo@users.noreply.github.com>
2020-01-24 18:16:37 +0100

    Comentado de funciones y preparación de lokalise

--- Bienestapp/Background/serverRetriever.swift
+++ Bienestapp/Background/serverRetriever.swift
@@ -8,25 +8,36 @@
 
 import UIKit
 
+// se declaran fuera de la clase para que sean unas variables publicas y accesibles
 var nameArray: Array<String> = []
 var optionsArray: Array<String> = []
 var imageURLArray: Array<String> = []
 
 class serverRetriever: UIViewController {
     
+    // array genérico para meter la respuesta del get
     var jsonArray: NSArray?
+    
+    // llamada al gestor de respuestas
     var HttpMessenger = HTTPMessenger()
 
+    // tras pedir información al server, la traduce a Arrays y luego los guarda en divisiones
     func infoGatherer(thisCollectionView: UICollectionView) {
         
+        // realiza el get
         let get = self.HttpMessenger.get(endpoint: "times")
         
+        // recoge la respuesta
         get.responseJSON { response in
             
+            // se cerciona de que haya respuesta
             if let JSON = response.result.value {
                 
+                // pasa el JSON a array
                 self.jsonArray = JSON as? NSArray
                 
+                // bucle que desgrana el array del JSON en los arrays que necesitamos según
+                // el índice de
                 for item in self.jsonArray! as! [NSDictionary] {
                     
                     let name = item["name"] as! String
@@ -37,6 +48,7 @@ class serverRetriever: UIViewController {
                     optionsArray.append(options)
                     imageURLArray.append(imageURL)
                 }
+                // recarga la vista para hacer efectivos los cambios
                 thisCollectionView.reloadData()
             }
         }
